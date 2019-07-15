8b6c24e62221d8c8ac6eac42c3d697a0d23beef5
GabrielSalvadorCardoso <gabrielcardoso95@gmail.com>
2019-07-14 22:06:26 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import GeoJSON from 'ol/format/GeoJSON';
+import Vector from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import './openLayers/css/default.css';
+import axios from 'axios';
+import {bbox} from 'ol/loadingstrategy';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class BackgroundMap {
+  constructor(data=null){
+    this.map = new Map({
+      target: "map",
+      view: new View({
+        center: [-6031024.58685793, -1976355.8033415168],
+        zoom:7,
+        zoomFactor:1.5,
+        minZoom:2
+      }),
+      layers: [
+        new TileLayer({source: new OSM()})
+      ]
+    })
+    
+  }
+
+  appendLayer(url) {
+    const vector_layer = new VectorLayer({
+      source: new Vector({
+        url: url,
+        format: new GeoJSON()
+      })
+    });
+    this.map.addLayer(vector_layer);
+  }
 }
 
-export default App;
+function MapContainer(props) {
+  //const [mapState, mapUpdater] = useState(null);
+  const mapViewContainer = (<div  id="map"
+                                  style={{
+                                    position: "absolute",
+                                    width: "100%",
+                                    height: "100%",
+                                    bottom: 0,
+                                    zindex: 0
+                                  }}></div>);  
+  useEffect(() => {
+    let backgroundMap = new BackgroundMap();
+    backgroundMap.appendLayer(props.layerUrl);    
+  }, []);
+
+  return mapViewContainer; 
+}
+
+class MapContainerWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {layerUrls: props.layerUrls}
+    console.log(this.state);
+  }
+
+  render() {
+    return (<MapContainer layerUrl={this.state.layerUrls[0]} />);
+  }
+}
+
+export default MapContainerWrapper;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import MapContainer from './App';
 import * as serviceWorker from './serviceWorker';
+import MapContainerWrapper from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<MapContainerWrapper layerUrls={['http://192.168.0.11:30000/api/bcim/aldeias-indigenas/']} />,
+    document.getElementById('root')
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
